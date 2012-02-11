aad36b7059b09934bebae92eed0cfd5906aef338
Chris Boyle <chris@bbck.net>
2012-02-10 19:30:47 -0500

    Very basic user modeling with only an email.

--- auction_notifier.rb
+++ auction_notifier.rb
@@ -1,3 +1,12 @@
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :email, String, required: true, format: :email_address, unique: true
+end
+
+DataMapper.finalize
+
 get '/' do
   "Hello World!"
 end
--- spec/auction_notifier_spec.rb
+++ spec/auction_notifier_spec.rb
@@ -5,4 +5,35 @@ describe 'Auction Notifier App' do
     get '/'
     last_response.should be_ok
   end
+
+  describe 'users' do    
+    before { @user = User.new(email: "email@example.com") }
+    subject { @user }
+
+    it { should respond_to(:email) }
+
+    it { should be_valid }
+
+    describe "when email is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
+    end
+
+    describe "when email is invalid" do
+      addresses =  %w{email@example,com email@example email_at_example.com @example.com}
+      addresses.each do |address|
+        before { @user.email = address }
+        it { should_not be_valid }
+      end
+    end
+
+    describe "when email is taken" do
+      before do 
+        same_user = User.new(email: "email@example.com")
+        same_user.save
+      end
+
+      it { should_not be_valid }
+    end
+  end
 end
