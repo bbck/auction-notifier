563c66ea4aec436645dea9e9e710755545e667ae
Chris Boyle <chris@bbck.net>
2012-02-13 19:04:31 -0500

    Change test database from file to memory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ set :raise_errors, true
 set :logging, false
 set :views => File.join(File.dirname(__FILE__), "..", "views")
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/test.db")
+DataMapper.setup(:default, "sqlite::memory:")
 
 def app
   Sinatra::Application
